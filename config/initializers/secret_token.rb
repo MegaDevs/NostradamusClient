9f2391de8cf95eafa8233fe0655e9d5a6f1f3d2e
sirnicolaz <sirnicolaz@gmail.com>
2012-11-17 22:05:33 +0100

    The Beginning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nostradamus::Application.config.secret_token = '325288a27c33a2a62216db595fe764c6d32ff073754e909de782dcb3429b929add2d28f29265beed937204ea1f37b06dbf3d46a977f048ae4aa5ef4c1f592142'
