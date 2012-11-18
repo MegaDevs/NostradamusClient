0d72d1e6ec9f5efde5d76ace8a5859689d0522c1
sirnicolaz <sirnicolaz@gmail.com>
2012-11-18 12:52:29 +0100

    Add missing stuff

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,38 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+function initializeMap(coords) {  
+    var latlng = new google.maps.LatLng(coords[0]['lat'], coords[0]['lat']);  
+    var myOptions = {  
+      zoom: 7,  
+      center: latlng,  
+      mapTypeId: google.maps.MapTypeId.ROADMAP  
+    };  
+      
+    var map = new google.maps.Map(document.getElementById("map_canvas"),  
+        myOptions);  
+  
+    // Creating a marker and positioning it on the map 
+    for (var i = coords.length - 1; i >= 0; i--) {
+       	var marker = new google.maps.Marker({    
+      		position: new google.maps.LatLng(coords[i]['lat'], coords[i]['lon']),    
+      		map: map    
+    	}); 
+    };      
+}
+
+
+function loadUsers(){
+	$.getJSON('http://nostradamus-whymca.appspot.com/get_nearby_users?&callback=?', function(json) { 
+  		coords = [];
+  		for (var i = json.length - 1; i >= 0; i--) {
+  			coords[i] = { 'lat' : json[i].latitude, 'lon' : json[i].longitude };
+  		};
+
+  		initializeMap(coords);
+	});
+}
+
+$(document).ready(function(){
+	loadUsers();
+});
